e5872e53c5f24062f3a278bb548da66cfd8b0bc6
Jose Camacho <06-39296@usb.ve>
2019-04-19 00:23:44 -0400

    Week 2: Single Page Application Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  dish: Dish;
+  promotion: Promotion;
 
-  constructor() { }
+  constructor(private dishService: DishService, private promotionService: PromotionService) { }
 
   ngOnInit() {
+    this.promotion = this.promotionService.getFeaturedPromotion();
+    this.dish = this.dishService.getFeaturedDish();
   }
 
 }
