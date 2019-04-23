77e6c47307c6ce97850fb64f8f549a472c57e7b5
Jose Camacho <06-39296@usb.ve>
2019-04-23 15:34:20 -0400

    Week 3 Assignment: Angular Forms and Reactive Programming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
@@ -63,6 +64,7 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatSliderModule,
     MatProgressSpinnerModule
   ],
   providers: [
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
@@ -14,11 +16,34 @@ export class DishdetailComponent implements OnInit {
   dish: Dish;
   dishIds: string[];
   prev: string;
-  next: string; 
+  next: string;
+  date: string;
+  //Form
+  commentForm: FormGroup;
+  newcomment: Comment;
+  @ViewChild('cform') feedbackFormDirective;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required': 'Author Name is required.',
+      'minlength': 'Author Name must be at least 2 characters long.'
+    },
+    'comment': {
+      'required': 'Comment field is required.'
+    }
+  }
 
   constructor(private dishService: DishService,
               private route: ActivatedRoute,
-              private location: Location) { }
+              private location: Location,
+              private c: FormBuilder) {
+                this.createForm();
+              }
 
   ngOnInit() {
     this.dishService.getDishIds()
@@ -27,6 +52,53 @@ export class DishdetailComponent implements OnInit {
         .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
         .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
+
+  createForm(){
+    this.commentForm = this.c.group({
+      author: ['', [Validators.required, Validators.minLength(2) ]],
+      rating: 5,
+      comment: ['', Validators.required ]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); //(re)set validation messages now
+  }
+
+  onValueChanged(data?: any) {
+    if(!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          if (control.errors.hasOwnProperty(key)) {
+            this.formErrors[field] += messages[key] + ' ';
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit(){
+    this.newcomment = this.commentForm.value;
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+    this.feedbackFormDirective.resetForm({
+      'rating': 5
+    });
+
+    let d = new Date();
+    this.newcomment.date = d.toISOString();
+    this.dish.comments.push(this.newcomment);
+  }
+
   setPrevNext(dishId: string) {
     const index = this.dishIds.indexOf(dishId);
     this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
